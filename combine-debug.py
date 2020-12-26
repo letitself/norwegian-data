ced98e1949aeef40ac6cf48eee8914047902ffae
Radovan Bast <bast@users.noreply.github.com>
2020-12-26 16:57:22 +0100

    also create a shorter version for debugging purposes

---
+++ combine-debug.py
@@ -0,0 +1,12 @@
+import glob
+
+print("---")
+for i, yaml_file in enumerate(glob.glob("data/*.yml")):
+    if i < 20:
+        with open(yaml_file, "r") as f:
+            for i, line in enumerate(f.read().splitlines()):
+                if i == 1:
+                    record = line.split()[-1]
+                    print(f"{record}:")
+                if i > 1:
+                    print(f"  {line}")
