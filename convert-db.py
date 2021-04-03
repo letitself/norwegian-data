814fedfbc3d5e9b678dc78d93984de7c8c24d30f
Radovan Bast <bast@users.noreply.github.com>
2021-04-03 14:57:59 +0200

    do not split dependency_structure_of_illustration on ","

--- convert-db.py
+++ convert-db.py
@@ -6,19 +6,13 @@ def record_to_filename(record):
     return f"data/{str(record).zfill(4)}.yml"
 
 
-def split_string(s):
+def split_string(s, split_sequence):
     if s.strip() == "":
         return ["~"]
-    if "+" in s:
-        return [w.strip() for w in s.split("+")]
-    if "," in s:
-        return [w.strip() for w in s.split(",")]
-    if ";" in s:
-        return [w.strip() for w in s.split(";")]
-    elif "/" in s:
-        return [w.strip() for w in s.split("/")]
-    else:
-        return [s.strip()]
+    for character in split_sequence:
+        if character in s:
+            return [w.strip() for w in s.split(character)]
+    return [s.strip()]
 
 
 def normalize_usage_label(s):
@@ -30,9 +24,9 @@ def normalize_usage_label(s):
     return s
 
 
-def print_as_yaml_list(string_spreadsheet):
+def print_as_yaml_list(string_spreadsheet, split_sequence):
     s = ""
-    for line in split_string(string_spreadsheet):
+    for line in split_string(string_spreadsheet, split_sequence):
         if ":" in line or "[" in line or "]" in line:
             s += f"  - '{line}'\n"
         else:
@@ -44,6 +38,8 @@ def write_record(row, record):
     tags = "Actionality,Actuality,Additive,Addressee,Apprehension,Assessment,Attitude,Calculation,Caritive,Causation,Cause,Comitative,Comparison,Condition,Concession,Consequence,Degree of accuracy,Degree of intensity,Discourse structure,Epistemic modality (Degree of certainty),Exceptive,Exclusive,Inclusive,Instrument,Manner,Measure,Mirative,Non-existence,Non-Standard Subject,Options,Phase of action,Pluractionality,Polarity value,Possession,Prohibition,Purpose,Quantification,Reaction to the previous discourse,Request,Result,Root modality,Routine,Salient property,Source of information,Source of opinion,Spatial expression,Subset,Taxis,Temporal expression,Temporary characteristics,Timeline,Threat,Volition".split(
         ","
     )
+
+    split_sequence = ["+", ",", ";", "/"]
 
     with open(record_to_filename(record), "w") as f:
         f.write("---\n")
@@ -65,31 +61,33 @@ def write_record(row, record):
                 f.write("  - |\n")
                 f.write(f"       {entry}\n")
         f.write("morphology:\n")
-        f.write(print_as_yaml_list(row["Morphology"]))
+        f.write(print_as_yaml_list(row["Morphology"], split_sequence))
         f.write("syntactic_type_of_construction:\n")
-        f.write(print_as_yaml_list(row["Synt. type of construction"]))
+        f.write(print_as_yaml_list(row["Synt. type of construction"], split_sequence))
         f.write("syntactic_function_of_anchor:\n")
-        f.write(print_as_yaml_list(row["Synt. func. of anchor"]))
+        f.write(print_as_yaml_list(row["Synt. func. of anchor"], split_sequence))
         f.write("syntactic_structure_of_anchor:\n")
-        f.write(print_as_yaml_list(row["Synt. structure of anchor"]))
+        f.write(print_as_yaml_list(row["Synt. structure of anchor"], split_sequence))
         f.write("part_of_speech_of_anchor:\n")
-        f.write(print_as_yaml_list(row["Part of speech of anchor"]))
+        f.write(print_as_yaml_list(row["Part of speech of anchor"], split_sequence))
         f.write("semantic_roles:\n")
-        f.write(print_as_yaml_list(row["Semantic role"]))
+        f.write(print_as_yaml_list(row["Semantic role"], split_sequence))
         f.write("intonation:\n")
         f.write(
             print_as_yaml_list(
-                row["Communicative type (only for Clause and Biclausal)"]
+                row["Communicative type (only for Clause and Biclausal)"],
+                split_sequence,
             )
         )
         f.write(f"usage_label: {normalize_usage_label(row['Usage label'])}\n")
 
         f.write("dependency_structure:\n")
-        f.write(print_as_yaml_list(row["Dependency Structure"]))
+        f.write(print_as_yaml_list(row["Dependency Structure"], split_sequence))
         f.write("dependency_structure_of_illustration:\n")
         f.write(
             print_as_yaml_list(
-                row["Dependency Structure of Illustration"].replace("\\n", " ")
+                row["Dependency Structure of Illustration"].replace("\\n", " "),
+                ["+"],
             )
         )
 
