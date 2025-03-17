0d852cf19325c584124a77b71574e515898a2062
zojabutenko <zoja.butenko@yandex.ru>
2025-03-17 12:17:01 +0100

    upd quote escape in illustrations

--- convert-db.py
+++ convert-db.py
@@ -47,7 +47,11 @@ def write_record(row, record):
         f.write(f"record: {record}\n")
         f.write(f"name: \"{row['Name'].strip()}\"\n")
         f.write(f"UD_name: '{row['Name UD'].strip()}'\n")
-        f.write(f"illustration: \"{row['Illustration'].strip()}\"\n")
+
+        # f.write(f"illustration: \"{row['Illustration'].strip()}\"\n")
+        f.write("illustration: |\n")
+        f.write(f"    {row['Illustration'].strip()}\n")
+
         f.write(f"cefr_level: {row['CEFR level'].strip()}\n")
         f.write("definitions:\n")
         for language in ["Norwegian", "English"]:
